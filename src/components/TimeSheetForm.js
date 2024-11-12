36ad7a8de2ae36b9197fd0c9cb5e867d9566cc82
Nicola Candeloro <candeloro.nicolo@gmail.com>
2024-11-12 17:07:52 +0100

    msg

--- src/components/TimeSheetForm.js
+++ src/components/TimeSheetForm.js
@@ -4,7 +4,40 @@ import React from 'react'
 const TimeSheetForm = () => {
     return (
         <>
-           
+            <div className='container-fluid'>
+                <form className='myform'>
+                    <div className='inputs'>
+                        <div className='input'>
+
+                            <input className='date-time'>
+
+
+
+                            </input>
+                        </div>
+                        <div className='input'>
+
+                            <input className='date-time'>
+
+
+
+                            </input>
+                        </div>
+                        <div className='input'>
+                            <input>Activity</input>
+
+                        </div>
+                        <div className='input'>
+                            <input>Description</input>
+
+                        </div>
+
+
+
+                    </div>
+                </form>
+            </div>
+
         </>
     )
 }
